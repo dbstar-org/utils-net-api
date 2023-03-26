976d60c668857a6715930003647a5f3356c28646
dbstarll <dbstarll@gmail.com>
2023-03-26 22:46:24 +0800

    request输出debug信息

--- src/main/java/io/github/dbstarll/utils/net/api/ApiAsyncClient.java
+++ src/main/java/io/github/dbstarll/utils/net/api/ApiAsyncClient.java
@@ -3,6 +3,7 @@ package io.github.dbstarll.utils.net.api;
 import io.github.dbstarll.utils.net.api.index.Index;
 import org.apache.commons.io.IOUtils;
 import org.apache.hc.client5.http.async.HttpAsyncClient;
+import org.apache.hc.core5.concurrent.CallbackContribution;
 import org.apache.hc.core5.concurrent.FutureCallback;
 import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.ContentType;
@@ -42,6 +43,72 @@ public abstract class ApiAsyncClient extends AbstractApiClient<HttpAsyncClient> 
         }
     }
 
+    /**
+     * Triggered to signal receipt of a response message head.
+     *
+     * @param request       the request
+     * @param response      the response message head.
+     * @param entityDetails the response entity details or {@code null} if the response
+     *                      does not enclose an entity.
+     * @throws HttpException HttpException
+     * @throws IOException   IOException
+     */
+    @SuppressWarnings("RedundantThrows")
+    protected void consumeResponse(final ClassicHttpRequest request, final HttpResponse response,
+                                   final EntityDetails entityDetails) throws HttpException, IOException {
+        final String traceEntity = format(entityDetails);
+        logger.trace("response: [{}]@{} with {}:{}", request, request.hashCode(), response.getCode(), traceEntity);
+    }
+
+    /**
+     * Triggered to pass incoming data to the data consumer.
+     *
+     * @param request       the request
+     * @param entityDetails the response entity details or {@code null} if the response
+     *                      does not enclose an entity.
+     * @param src           data source.
+     * @throws IOException IOException
+     */
+    @SuppressWarnings("RedundantThrows")
+    protected void consume(final ClassicHttpRequest request, final EntityDetails entityDetails, final ByteBuffer src)
+            throws IOException {
+        logger.trace("consume: [{}]@{} with [{}]:{} bytes", request, request.hashCode(), entityDetails.getContentType(),
+                src.remaining());
+    }
+
+    /**
+     * Triggered to pass incoming data packet to the data consumer.
+     *
+     * @param request the request
+     * @param result  the data packet.
+     * @param <T>     type of result
+     * @param <I>     Index of T
+     * @return result
+     */
+    protected <T, I extends Index<T>> I stream(final ClassicHttpRequest request, final I result) {
+        logger.trace("stream: [{}]@{} with {}:{}[{}]", request, request.hashCode(),
+                result.getData().getClass().getName(), result.getIndex(), result.getData());
+        return result;
+    }
+
+    /**
+     * 在获取请求结果之后，对结果进行包装.
+     *
+     * @param request the request to execute
+     * @param result  请求结果
+     * @param <T>     请求结果类型
+     * @return 请求结果
+     */
+    protected <T> T completed(final ClassicHttpRequest request, final T result) {
+        if (result != null) {
+            logger.trace("completed: [{}]@{} with {}:[{}]", request, request.hashCode(),
+                    result.getClass().getName(), result);
+        } else {
+            logger.trace("completed: [{}]@{} with null", request, request.hashCode());
+        }
+        return result;
+    }
+
     protected final <T> Future<T> execute(final ClassicHttpRequest request,
                                           final AsyncResponseConsumer<T> responseConsumer,
                                           final FutureCallback<T> callback) throws IOException, ApiException {
@@ -50,21 +117,42 @@ public abstract class ApiAsyncClient extends AbstractApiClient<HttpAsyncClient> 
         traceRequest(request);
 
         return httpClient.execute(buildRequestProducer(request), new AsyncResponseConsumerWrapper<T>(responseConsumer) {
+            private volatile EntityDetails entityDetails;
+
             @Override
             public void consumeResponse(final HttpResponse response, final EntityDetails entityDetails,
                                         final HttpContext context, final FutureCallback<T> resultCallback)
                     throws HttpException, IOException {
-                final String traceEntity = format(entityDetails);
-                logger.trace("response: [{}]@{} with {}", request, request.hashCode(), traceEntity);
+                this.entityDetails = entityDetails;
+                ApiAsyncClient.this.consumeResponse(request, response, entityDetails);
                 super.consumeResponse(response, entityDetails, context, resultCallback);
             }
 
             @Override
             public void consume(final ByteBuffer src) throws IOException {
-                logger.trace("consume: [{}]@{} with {} bytes", request, request.hashCode(), src.remaining());
+                final int position = src.position();
+                try {
+                    ApiAsyncClient.this.consume(request, entityDetails, src);
+                } finally {
+                    src.position(position);
+                }
                 super.consume(src);
             }
-        }, null, null, callback);
+
+            @Override
+            public void releaseResources() {
+                this.entityDetails = null;
+                super.releaseResources();
+            }
+        }, null, null, new CallbackContribution<T>(callback) {
+            @Override
+            public void completed(final T result) {
+                final T finalResult = ApiAsyncClient.this.completed(request, result);
+                if (callback != null) {
+                    callback.completed(finalResult);
+                }
+            }
+        });
     }
 
     protected final <T> Future<T> execute(final ClassicHttpRequest request,
@@ -86,7 +174,13 @@ public abstract class ApiAsyncClient extends AbstractApiClient<HttpAsyncClient> 
             final StreamFutureCallback<T, I> callback) throws IOException, ApiException {
         notNull(responseHandler, "responseHandler is null");
         notNull(callback, "callback is null");
-        return execute(request, StreamResponseHandlerResponseConsumer.create(responseHandler, callback), callback);
+        final StreamFutureCallback<T, I> myCallback = new StreamCallbackContribution<T, I>(callback) {
+            @Override
+            public void stream(final I result) {
+                super.stream(ApiAsyncClient.this.stream(request, result));
+            }
+        };
+        return execute(request, StreamResponseHandlerResponseConsumer.create(responseHandler, myCallback), myCallback);
     }
 
     protected final <T, I extends Index<T>> Future<List<T>> execute(
